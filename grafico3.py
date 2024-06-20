801970143feafc087ce29538ca6e1b90c245adab
JorgePaez30 <jorgepaez1103@gmail.com>
2024-06-20 00:39:58 -0500

    main

--- grafico3.py
+++ grafico3.py
@@ -7,6 +7,8 @@ with open('datos.json', 'r', encoding='utf-8') as file:
     datos = json.load(file)
 
 df_especies = pd.DataFrame(datos)
+
+# Cantidad de especies en Cundinamarca por tipo
 df_cundinamarca = df_especies[df_especies['Habitat'].str.contains('Cundinamarca', case=False, na=False)]
 def contar(nombre, vec):
   #Para esta funcion nombre es el criterio para contar (nombre) y vec es la lista donde contar
@@ -29,4 +31,30 @@ for elemento in lista_tipo:
 fig, diagx = plt.subplots()
 diagx.bar(lista_tipo,lista_valores)
 fig.savefig('Cantidad_especies_cundinamarca.png')
+plt.show()
+
+
+# Cantidad de especies en Boyacapor tipo
+df_boyaca= df_especies[df_especies['Habitat'].str.contains('Boyaca', case=False, na=False)]
+def contar(nombre, vec):
+  #Para esta funcion nombre es el criterio para contar (nombre) y vec es la lista donde contar
+  contador=0
+  for i in vec:
+    if i==nombre:
+      contador+=1
+  return contador
+#etiquetas únicas en tipo
+lista_tipo = df_boyaca['Tipo'].unique()
+lista_habitat = df_boyaca['Habitat'].unique()
+lista_tipo_especie = list(df_boyaca['Tipo'])
+lista_tipo_habitat = list(df_boyaca['Habitat'])
+#Armar otra lista con el conteo
+lista_valores=[]
+for elemento in lista_tipo:
+  cantidad=contar(elemento,lista_tipo_especie)
+  lista_valores.append(cantidad)
+#Diagrama de barras
+fig, diagx = plt.subplots()
+diagx.bar(lista_tipo,lista_valores)
+fig.savefig('Cantidad_especies_boyaca.png')
 plt.show()
